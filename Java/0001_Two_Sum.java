212465fa8ded9f17e90e80bf339f1678826c93be
Can <62196202+Wolverine-Can@users.noreply.github.com>
2020-06-02 23:43:15 -0700

    Update 0001_Two_Sum.java

--- Java/0001_Two_Sum.java
+++ Java/0001_Two_Sum.java
@@ -1,6 +1,6 @@
-# One pass HashMap solution
-# Time complexity: O(n)
-# Space complexity: O(n)
+// One pass HashMap solution
+// Time complexity: O(n)
+// Space complexity: O(n)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
